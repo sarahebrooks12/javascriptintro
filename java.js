15dae6660cee4b9600b59ccc73e29afe8a822afc
sarahebrooks12 <sarahebrooks12@gmail.com>
2020-03-19 18:08:09 -0400

    Newfiles

---
+++ java.js
@@ -0,0 +1,9 @@
+var firstName = "Sarah";
+var lastName = "Brooks";
+var age = "30";
+var likesTravel = false;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(likesTravel);
