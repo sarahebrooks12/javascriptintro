445ee59c5c2b492d2250b29d953c6b877c72ba53
sarahebrooks12 <sarahebrooks12@gmail.com>
2020-03-19 19:02:12 -0400

    Lightning Exercises

--- java.js
+++ java.js
@@ -1,9 +1,18 @@
-var firstName = "Sarah";
-var lastName = "Brooks";
-var age = "30";
-var likesTravel = false;
+// In your JavaScript file, instantiate and assign a variable called movieArray. This variable should hold an array of your favorite movie titles. (Don't go overboard- two or three is fine).
+// Log each movie in the array to the console.
 
-console.log(firstName);
-console.log(lastName);
-console.log(age);
-console.log(likesTravel);
+
+var movieArray = ["Star Wars: The Last Jedi", "The Little Mermaid", "Guardians of the Galaxy Vol 1" ]
+console.log(movieArray)
+
+
+// Instead of storing the data from your first lightning exercise in separate variables, store them in an object. Your object should have a property of firstName, lastName, age, likesToTravel. It should also have a property called favoriteMovies, which should hold the array of your favorite movies you created in the last lightning exercise.
+
+var sarahObject = {
+    firstName: "Sarah",
+    lastName: "Brooks",
+    age: 30,
+    likesToTravel: false,
+    favoriteMovies: ["Star Wars: The Last Jedi", "The Little Mermaid", "Guardians of the Galaxy:Vol 1"]
+}
+console.log(sarahObject)
